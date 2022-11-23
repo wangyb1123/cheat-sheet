9ff949fe9270f5b886328dcc6f272434fa206cf6
wangyb1123 <37034642+wangyb1123@users.noreply.github.com>
2022-11-23 09:40:16 +0800

    docker installation script

---
+++ docker/install_docker_desktop_ubuntu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "####################################clean up exisiting docker desktop installation####################################"
+sudo apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge -y docker-desktop
+
+echo "####################################setup repository####################################"
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+echo "####################################install docker desktop####################################"
+wget -O docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.14.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
+sudo apt-get update
+sudo apt-get -y install ./docker-desktop.deb
+
+
+echo "####################################launch docker desktop####################################"
+systemctl --user start docker-desktop
+docker compose version
+docker version
+
+echo "####################################enable docker desktop to start on login####################################"
+systemctl --user enable docker-desktop
+systemctl --user stop docker-desktop
